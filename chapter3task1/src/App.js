58f3e4e8f52c07f96b8edf510ff3cd1261ee0b50
KimK8 <xxlbomb@gmail.com>
2022-02-15 15:09:35 +0200

    new project added

---
+++ chapter3task1/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="h1color">Welcome to my Site</h1>
+      <p className="psize">This is my first React App</p>
+    </div>
+  );
+}
+
+export default App;
